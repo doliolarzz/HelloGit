6e62ec7fb9c43d530cc141e3cd71623aa424c9dd
Warit Prasartritha <doliolarzz@Warits-MacBook-Pro.local>
2017-08-25 12:09:55 +0700

    Display Greeting message when program is excuted

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String[] args) {
+		greet("Warit Prasartritha");
+	}
+	
+	private static void greet(String name){
+		System.out.println("Hello, Git");
+		System.out.println("I am " + name );
+	}
+}
